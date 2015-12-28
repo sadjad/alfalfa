e19aabdecf2b39fa4719709fb0b099b5aeb027d7
Deepak Narayanan <deepakn94@gmail.com>
2015-12-28 06:49:09 +0000

    Additional statistics that could be potentially be useful

--- src/frontend/play-stats.cc
+++ src/frontend/play-stats.cc
@@ -32,15 +32,31 @@ int main( int argc, char const *argv[] )
     size_t track_id = 0;
     size_t frame_index = 0;
 
+    size_t num_frames_track_cheaper = 0;
+    size_t num_frames_switch_cheaper = 0;
+
+    size_t min_cost;
+
     auto frames = video.get_frames( track_id );
     for ( auto frame = frames.first; frame != frames.second; frame++ ) {
       if ( not frame->target_hash().shown ) continue;
       cout << frame_index++ << ",";
-      auto ts = player.get_min_track_seek( frame->target_hash().output_hash );
-      auto ss = player.get_min_switch_seek( frame->target_hash().output_hash );
+      auto ts_cost = get<0>( player.get_min_track_seek( frame->target_hash().output_hash ) ).cost;
+      auto ss_cost = get<0>( player.get_min_switch_seek( frame->target_hash().output_hash ) ).cost;
 
-      cout << get<0>( ts ).cost << "," << get<0>( ss ).cost << endl;
+      if ( ts_cost > ss_cost ) {
+        min_cost = ss_cost;
+        num_frames_switch_cheaper++;
+      } else {
+        min_cost = ts_cost;
+        num_frames_track_cheaper++;
+      }
+
+      cout << ts_cost << "," << ss_cost << "," << min_cost << endl;
     }
+
+    cout << endl << endl << "Switch cheaper " << num_frames_switch_cheaper << " times" << endl;
+    cout << "Backtracking in track cheaper " << num_frames_track_cheaper << " times" << endl;
 
   } catch ( const exception & e ) {
     print_exception( argv[ 0 ], e );
