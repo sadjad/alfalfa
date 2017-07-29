338456f4c004dff7d9fe2ad3b17b8c12c9349fff
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-29 10:22:14 -0700

    Implement y4mplay

--- src/frontend/y4mplay.cc
+++ src/frontend/y4mplay.cc
@@ -5,7 +5,8 @@
 
 #include "player.hh"
 #include "display.hh"
-#include "enc_state_serializer.hh"
+#include "yuv4mpeg.hh"
+#include "frame_input.hh"
 
 using namespace std;
 
@@ -13,20 +14,25 @@ int main( int argc, char *argv[] )
 {
   try {
     if ( argc < 2 ) {
-      cerr << "Usage: " << argv[ 0 ] << " FILENAME [decoder_state]" << endl;
+      cerr << "Usage: " << argv[ 0 ] << " FILENAME" << endl;
       return EXIT_FAILURE;
     }
 
-    Player player = argc < 3
-      ? Player( argv[ 1 ] )
-      : EncoderStateDeserializer::build<Player>(argv[2], argv[1]);
+    string filename { argv[ 1 ] };
 
-    VideoDisplay display { player.example_raster() };
+    shared_ptr<FrameInput> input_reader;
 
-    while ( not player.eof() ) {
-      display.draw( player.advance() );
-      cerr << "Displaying frame #" << player.cur_frame_no() << "...";
-      getchar();
+    if ( filename == "-" ) {
+      input_reader = make_shared<YUV4MPEGReader>( FileDescriptor( STDIN_FILENO ) );
+    }
+    else {
+      input_reader = make_shared<YUV4MPEGReader>( filename );
+    }
+
+    VideoDisplay display { input_reader->get_next_frame().get() };
+
+    while ( true ) {
+      display.draw( input_reader->get_next_frame().get() );
     }
   } catch ( const exception & e ) {
     print_exception( argv[ 0 ], e );
